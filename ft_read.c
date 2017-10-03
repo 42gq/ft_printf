fbf7769025764f48db0809afccde213c11b8fdb0
Guillaume QUERRE <gquerre@student.42.fr>
2017-10-04 00:31:11 +0200

    ui

--- ft_read.c
+++ ft_read.c
@@ -6,7 +6,7 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/20 02:44:18 by gquerre           #+#    #+#             */
-/*   Updated: 2017/10/03 10:54:34 by gquerre          ###   ########.fr       */
+/*   Updated: 2017/10/04 00:29:53 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int		ft_read(const char *format, va_list arg, t_env *e)
 		if (i < 0 || e->error != 0)
 			return (ft_error_printf(e->error));
 	}
-	ft_putstr(e->stock);
+	write(1, e->stock, e->size);
 	free(tmp);
 	return (e->size);
 }
